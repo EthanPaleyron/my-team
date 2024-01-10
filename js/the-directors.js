82c38da4904d39a3838f5cf1926cf86ac4e46054
EthanPaleyron <epaleyron@edenschool.fr>
2024-01-10 19:35:48 +0100

    end

--- js/the-directors.js
+++ js/the-directors.js
@@ -11,35 +11,35 @@ let directors = [
     name: "Christian Duncan",
     work: "Co-founder & COO",
     description:
-      "“It always amazes me how much talent there is in every corner of the globe.”",
+      "“Distributed teams required unique processes. You need to approach work in a new way.”",
   },
   {
     img: "cruz.jpg",
     name: "Cruz Hamer",
     work: "Co-founder & CTO",
     description:
-      "“It always amazes me how much talent there is in every corner of the globe.”",
+      "“Technology is at the forefront of enabling distributed teams. That's where we come in.”",
   },
   {
     img: "drake.jpg",
     name: "Drake Heaton",
     work: "Business Development Lead",
     description:
-      "“It always amazes me how much talent there is in every corner of the globe.”",
+      "“Hiring similar people from similar backgrounds is a surefire way to stunt innovation.”",
   },
   {
     img: "griffin.jpg",
     name: "Griffin Wise",
     work: "Lead Marketing",
     description:
-      "“Empowered teams create truly amazing products. Set the north star and let them follow it.”",
+      "“Unique perspectives shape unique products, which is what you need to survive these days.”",
   },
   {
     img: "aden.jpg",
     name: "Aden Allan",
     work: "Head of Talent",
     description:
-      "“It always amazes me how much talent there is in every corner of the globe.”",
+      "“Empowered teams create truly amazing products. Set the north star and let them follow it.”",
   },
 ];
 
@@ -65,15 +65,25 @@ for (const director in directors) {
                 </article>`;
 }
 
+let lastDirector;
 const buttons = document.querySelectorAll(".buttons_more_info");
-
 buttons.forEach((button, i) => {
   button.addEventListener("click", () => {
     let more_content_director = document.querySelector(
       ".more_content_directors_" + i
     );
     let content_directors = document.querySelector(".content_directors_" + i);
-    // content_directors.style.display = "none";
-    // more_content_director.style.display = "flex";
+    if (more_content_director.style.display == "flex") {
+      more_content_director.style.display = "none";
+      content_directors.style.display = "flex";
+      button.innerHTML =
+        '<img class="more_info" src="../assets/icon/icon-cross.svg" alt="More info">';
+    } else {
+      content_directors.style.display = "none";
+      more_content_director.style.display = "flex";
+      button.innerHTML =
+        '<img class="close_info" src="../assets/icon/icon-close.svg" alt="Close info">';
+      lastDirector = button.length;
+    }
   });
 });
